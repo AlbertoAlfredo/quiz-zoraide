eb2d2a3a69ae2a9f69b99a13bf997fadfc680144
Alberto Ferreira Alfredo <albertoalfredo.profissional@gmail.com>
2024-03-12 23:07:23 -0300

    Pré feito toda base do sistema de passa ou repassa

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+  import Perguntas from './components/Perguntas.vue';
+
+  const perguntas = [
+    {
+      titulo : "Titulo 1",
+      resposta1: "Resposta 01",
+      resposta2: "Resposta 02",
+      resposta3: "Resposta 03",
+      resposta4: "Resposta 04",
+      teste: 2
+    }
+  ]
+
+</script>
+
+<template>
+  <div class="container">
+
+    <Perguntas 
+      title="Teste"
+      resposta1="resposta 1"
+      resposta2="resposta 2"
+      resposta3="resposta 3"
+      resposta4="resposta 4"
+      correta = 1
+    />
+    <div  class="container text-center">
+    <div class="row align-items-center">
+      <div class="col">
+        <p><button @click="count--" type="button" class="btn btn-outline-secondary">Voltar</button></p>
+      </div>
+      <div class="col">
+        <p><button @click="count++" type="button" class="btn btn-outline-secondary">Próximo</button></p>
+      </div>
+    </div>
+  </div>
+  </div>
+
+
+</template>
+<style>
+</style>
+
+<style scoped>
+</style>
---
+++ src/components/Perguntas.vue
@@ -0,0 +1,58 @@
+<script setup>
+  import { ref } from 'vue'
+  defineProps(['title', 'resposta1', 'resposta2', 'resposta3', 'resposta4', 'correta'])
+
+  
+
+  var resposta = ref(false)
+</script>
+
+<template>
+<div class="container-fluid">
+  <header class="container">
+    <h1>Quiz Zoraide</h1>
+  </header>
+  <div class="container">
+    
+    
+
+    <h2>{{ title }}</h2>
+  <div v-if="!resposta" class="container text-center">
+    <div class="row align-items-center">
+      <div class="col">
+        <p><button @click="resposta = true" type="button" class="btn btn-outline-secondary">{{ resposta1 }}</button></p>
+        <p><button @click="resposta = true" type="button" class="btn btn-outline-secondary">{{ resposta2 }}</button></p>
+      </div>
+      <div class="col">
+        <p><button @click="resposta = true" type="button" class="btn btn-outline-secondary">{{ resposta3 }}</button></p>
+        <p><button @click="resposta = true" type="button" class="btn btn-outline-secondary">{{ resposta4 }}</button></p>
+      </div>
+    </div>
+  </div>
+
+
+  <div v-if="resposta"  class="container text-center">
+    <div class="row align-items-center">
+      <div class="col">
+        <p><button type="button" class="btn" :class="{'btn-danger': correta!=1, 'btn-success': correta==1}">{{ resposta1 }}</button></p>
+        <p><button type="button" class="btn" :class="{'btn-danger': correta!=2, 'btn-success': correta==2}">{{ resposta2 }}</button></p>
+
+      </div>
+      <div class="col">
+        <p><button type="button" class="btn" :class="{'btn-danger': correta!=3, 'btn-success': correta==3}">{{ resposta3 }}</button></p>
+        <p><button type="button" class="btn" :class="{'btn-danger': correta!=4, 'btn-success': correta==4}">{{ resposta4 }}</button></p>
+
+      </div>
+      
+      
+    </div>
+  </div>
+
+  </div>
+</div>
+</template>
+<style>
+</style>
+
+<style scoped>
+</style>
